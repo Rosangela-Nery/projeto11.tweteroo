9b9355c6e011c20e1710d3e57a47f8f7ae8b6a40
Rosângela Nery <rosangela2nery@gmail.com>
2022-08-29 16:06:13 -0300

    Implementando funcionalidades em backend

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { application } from 'express';
 import cors from 'cors';
 
 const server = express();
@@ -27,13 +27,44 @@ server.post('/sign-up', (req, res) => {
 
     const user = req.body;
     users.push(user);
-    res.send(user);
+    res.status(201).send('Ok');
+});
+
+server.get('/tweets/:name', (req, res) => {
+    const name = req.params.name
+    const filterTweet = tweets.filter(tweet => tweet.username === name)
+    const imgTweet = filterTweet.map(tweet => {
+        return {
+            ...tweet,
+            avatar: users.find(user => user.username === tweet.username).avatar
+        }
+    })
+    if(filterTweet.length === 0) {
+        return res.status(404).send('Usuário não identificado!')
+    }
+    return res.status(200).send(imgTweet)
 });
 
 server.get('/tweets', (req, res) => {
-    res.send(tweet)
+    const verificationLatestTweets = tweets.map((tweet) => {
+        return {
+            ...tweet,
+            avatar: users.find(user => user.username === tweet.username).avatar
+        }
+    })
+    res.send(verificationLatestTweets.slice(0, 10))
 });
 
-console.log(tweet)
+server.post('/tweets', (req, res) => {
+    const { username, tweet } = req.body
 
-server.listen(5000);
+    if(!username || !tweet) {
+        res.status(400).send('Todos os campos são obrigatórios!')
+    }
+
+    const publication = req.body;
+    tweets.unshift(publication);
+    res.status(201).send('Ok');
+});
+
+server.listen(5000, () => { console.log('Server rodando na porta 5000') });
